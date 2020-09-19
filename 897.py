736f8d63e2eb9c162eb5e8495346a87a106462fa
Geetha Kamath Koteshwar <37272001+geethakamath18@users.noreply.github.com>
2020-09-19 18:09:31 -0400

    LeetCode problem 897: Increasing Order Search Tree

--- 897.py
+++ 897.py
@@ -1,5 +1,4 @@
 #LeetCode problem 897: Increasing Order Search Tree
-# Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
 #         self.val = val
@@ -7,22 +6,21 @@
 #         self.right = right
 class Solution:
     def increasingBST(self, root: TreeNode) -> TreeNode:
-        if root is None:
-            return
+        res=[]
         if root.left is None and root.right is None:
             return TreeNode(root.val)
-        res=self.getPreOrder(root)
+        res=self.getPreOrder(root,res)
+        a=None
         a=newNode=TreeNode(res[0])
         for i in range(1,len(res)):
             newNode.right=TreeNode(res[i])
-            newNode.left=None
             newNode=newNode.right
         return a
     
-    def getPreOrder(self, root: TreeNode,res=[])->List[int]:
+    def getPreOrder(self, root: TreeNode,r)->List[int]:
         if root is None:
             return
-        self.getPreOrder(root.left,res)
-        res.append(root.val)
-        self.getPreOrder(root.right,res)
-        return res
+        self.getPreOrder(root.left,r)
+        r.append(root.val)
+        self.getPreOrder(root.right,r)
+        return r
