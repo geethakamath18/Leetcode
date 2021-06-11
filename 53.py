8e12e8e89f49f2740371814a9b9c854e8ffad5f9
Geetha Kamath Koteshwar <37272001+geethakamath18@users.noreply.github.com>
2021-06-11 19:20:10 -0400

    LeetCode problem 53: Maximum Subarray

---
+++ 53.py
@@ -0,0 +1,10 @@
+
+#LeetCode problem 53: Maximum Subarray
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        m = current_sum = nums[0]
+        for i in nums[1:]:
+            current_sum = max(i, current_sum+i)
+            print(current_sum)
+            m = max(m, current_sum)     
+        return m
